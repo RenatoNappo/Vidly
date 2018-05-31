860e3c18f3efc6c870aca2424ba7bebfa91e0aa9
RenatoNappo <31985727+RenatoNappo@users.noreply.github.com>
2018-05-31 16:22:16 +0100

    Initial module work

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ViewResult Random()
+        {
+            var movie = new Movie() { Name = "The Matrix" };
+            return View(movie);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+        // /movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("page index = {0} and sorting is by {1}",pageIndex, sortBy));
+        }
+
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
